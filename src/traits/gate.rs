de5bc3b2e1e42b9b231c669d548083acf63c4fe5
Ekrem BAL <mail.ekrembal@gmail.com>
2023-12-07 16:36:43 +0300

    Add gate response script

--- src/traits/gate.rs
+++ src/traits/gate.rs
@@ -2,7 +2,9 @@
 //use std::rc::Rc;
 //use std::cell::RefCell;
 
+use bitcoin::ScriptBuf;
+
 pub trait GateTrait {
     fn evaluate(&mut self);
-    fn create_challenge_script(&self) -> String;
+    fn create_response_script(&self, lock_hash: [u8; 32]) -> ScriptBuf;
 }
--- src/traits/wire.rs
+++ src/traits/wire.rs
@@ -1,5 +1,6 @@
-use bitcoin::{ScriptBuf, XOnlyPublicKey};
+use bitcoin::{ScriptBuf, XOnlyPublicKey, script::Builder};
 
 pub trait WireTrait {
     fn generate_anti_contradiction_script(&self, verifier_pk: XOnlyPublicKey) -> ScriptBuf;
+    fn add_bit_commitment_script(&self, builder: Builder) -> Builder;
 }
